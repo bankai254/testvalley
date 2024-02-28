e65245bcbf339b5ec4cb64a2d7873da82717fa22
Brian Kinyua <229225+bankai254@users.noreply.github.com>
2024-02-29 00:44:31 +0300

    feat: adds rollbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { ErrorBoundary,Provider as RollbarProvider } from '@rollbar/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
@@ -10,33 +11,53 @@ import '@/styles/globals.scss';
 
 import { fetcher } from '@/utils/api';
 
+const rollbarConfig = {
+  accessToken: process.env.NEXT_PUBLIC_ROLLBAR_CLIENT_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  enabled: true,
+  payload: {
+    client: {
+      javascript: {
+        code_version: '1.0.0',
+        source_map_enabled: true
+      }
+    },
+    environment: process.env.VERCEL_ENV || 'development'
+  }
+};
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfig
-      value={{
-        revalidateIfStale: false,
-        revalidateOnFocus: false,
-        revalidateOnReconnect: false,
-        fetcher: fetcher,
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        onError: (error: any) => {
-          if (error.status !== 403 && error.status !== 404) {
-            // We can send the error to Sentry,
-            // or show a notification UI.
+    <RollbarProvider config={rollbarConfig}>
+      <SWRConfig
+        value={{
+          revalidateIfStale: false,
+          revalidateOnFocus: false,
+          revalidateOnReconnect: false,
+          fetcher: fetcher,
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          onError: (error: any) => {
+            if (error.status !== 403 && error.status !== 404) {
+              // We can send the error to Sentry,
+              // or show a notification UI.
+            }
           }
-        }
-      }}
-    >
-      <Head>
-        <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width"
-        />
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </Head>
-      <Component {...pageProps} />
-      <SpeedInsights />
-    </SWRConfig>
+        }}
+      >
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width"
+          />
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
+        <ErrorBoundary>
+          <Component {...pageProps} />
+        </ErrorBoundary>
+        <SpeedInsights />
+      </SWRConfig>
+    </RollbarProvider>
   );
 }
--- src/pages/_error.jsx
+++ src/pages/_error.jsx
@@ -2,11 +2,30 @@ import React from 'react';
 
 import * as Sentry from "@sentry/nextjs";
 import Error from "next/error";
+import Rollbar from 'rollbar';
 
 const CustomErrorComponent = (props) => {
   // eslint-disable-next-line react/prop-types
   return <Error statusCode={props.statusCode} />;
 };
+
+Error.getInitialProps = ({ req, res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+  // Only require Rollbar and report error if we're on the server
+  if (!process.browser) {
+      console.log('Reporting error to Rollbar...')
+      const rollbar = new Rollbar(process.env.ROLLBAR_SERVER_TOKEN)
+      rollbar.error(err, req, (rollbarError) => {
+          if (rollbarError) {
+              console.error('Rollbar error reporting failed:')
+              console.error(rollbarError)
+              return
+          }
+          console.log('Reported error to Rollbar')
+      })
+  }
+  return { statusCode }
+}
 
 CustomErrorComponent.getInitialProps = async (contextData) => {
   // In case this is running in a serverless function, await this in order to give Sentry
